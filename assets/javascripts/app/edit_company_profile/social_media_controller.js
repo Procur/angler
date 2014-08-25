a7543ec75bf334feabfef57a4a1776c992a57d22
Adam Morgan <atom.morgan@gmail.com>
2014-08-25 10:30:45 -0400

    Add listener for emit to switch between buyer and supplier data

--- assets/javascripts/app/edit_company_profile/social_media_controller.js
+++ assets/javascripts/app/edit_company_profile/social_media_controller.js
@@ -4,14 +4,51 @@
     definitions;
 
   definitions = [
+    '$rootScope',
     '$scope',
+    'userService',
+    'buyerService',
+    'supplierService',
     socialMediaController
   ];
 
   angular.module('pc.EditCompanyProfile')
     .controller('socialMediaController', definitions);
 
-  function socialMediaController($scope) {
+  function socialMediaController($rootScope, $scope, user, buyer, supplier) {
+    var role;
+    $scope.user = {
+      activeMode: user.activeMode,
+      isBuyerMode: user.isBuyerMode,
+      isSupplierMode: user.isSupplierMode
+    };
+
+    $rootScope.$on('activeModeChanged', changeRole);
+
+    function changeRole(event, data) {
+      if (data === 'buyer') {
+        role = buyer;
+        getAttributes(role);
+      }
+      else {
+        role = supplier;
+        getAttributes(role);
+      }
+      console.log($scope.actor);
+    }
+
+    function getAttributes(role) {
+      $scope.actor = {
+        dba: role.get('dba'),
+        facebook: role.get('socialMedia.facebook'),
+        google: role.get('socialMedia.google+'),
+        instagram: role.get('socialMedia.instagram'),
+        linkedin: role.get('socialMedia.linkedin'),
+        pinterest: role.get('socialMedia.pinterest'),
+        twitter: role.get('socialMedia.twitter'),
+        tumblr: role.get('socialMedia.tumblr')
+      }
+    }
 
   }
 
